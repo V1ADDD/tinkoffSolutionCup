45b2b1c21f94d7abed8dc2bd71bd98744fbea8cc
V1ADDD <rusakvlad2@gmail.com>
2023-04-22 17:22:22 +0300

    finish

--- src/app/spendings/spendings.component.ts
+++ src/app/spendings/spendings.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 type Category = {
     id: number;
@@ -24,15 +25,30 @@ export class SpendingsComponent {
   categoryModel: any;
   categories: Category[];
   spendings: Spending[];
+  spentAll: number = 0;
+  datesOfSpends: string[] = [];
 
   constructor() {
     const categories: Category[] = JSON.parse(<string>localStorage.getItem("categories"));
     this.categories = categories.filter((x: Category) => x.user == localStorage.getItem("currentUser"));
     const spendings: Spending[] = JSON.parse(<string>localStorage.getItem("spendings"));
     this.spendings = spendings.filter((x: Spending) => x.user == localStorage.getItem("currentUser"));
+    for (const spend of this.spendings) {
+      this.datesOfSpends.push(spend.date.slice(-2));
+      this.spentAll += spend.spent;
+    }
   }
 
-  updateCategories(): void {
-
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      let summ: number = 0;
+      for (const spend of this.spendings) {
+        const dateOfSpend = new Date(spend.date);
+        if (dateOfSpend >= new Date(form.value.from) && dateOfSpend <= new Date(form.value.to)) {
+          summ += spend.spent;
+        }
+      }
+      alert("Потрачено за указанный период: "+summ);
+    }
   }
 }
